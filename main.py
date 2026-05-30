927ae2b00b323230c785592973b522cee58fc82a
olehog04-eng <olehog04@gmail.com>
2026-05-30 18:27:48 +0300

    Resolve stash conflicts and merge changes

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from utils import factorial, is_prime
 
 print(factorial(5))
 print(is_prime(7))
+
+from utils import factorial, is_prime, is_power_of_5
+
+print(is_power_of_5(25))
+
+print(is_power_of_2(16))
+
--- utils.py
+++ utils.py
@@ -11,3 +11,16 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+def is_power_of_5(n):
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
+def is_power_of_2(n):
+    if n < 1:
+        return False
+    while n % 2 == 0:
+        n //= 2
+    return n == 1
+>>>>>>> Stashed changes
